d65820b48a1b364076bc56cc1c75386df0ce383f
Andrew-Romanov <andrewromanov@yahoo.com>
2021-01-23 22:43:30 +0200

    Выключил запрет скролла body под открытой модалкой

--- js/modal.js
+++ js/modal.js
@@ -9,7 +9,9 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
-      document.body.classList.toggle('modal-open')
+      // Нужно включить в _common-styles.scss этот селектор, но тогда выключится скролл
+      // и body скакнёт вправо
+      // document.body.classList.toggle('modal-open')
       refs.modal.classList.toggle('is-hidden');
     }
   })();
